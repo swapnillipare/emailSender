2cf880f804af45dc1789ccdc28c7a2b47606697b
Swapnil Lipare <swapnil.lipare@nice.com>
2018-01-15 20:34:26 +0530

    First Comit

---
+++ src/com/swapnil/send/mail/EmailUtility.java
@@ -0,0 +1,49 @@
+package com.swapnil.send.mail;
+
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * 
+ * @author Swanil
+ *
+ */
+public class EmailUtility {
+	public static void sendEmail(String host, String port, final String userName, final String password,
+			String toAddress, String subject, String message) throws AddressException, MessagingException {
+
+		Properties properties = new Properties();
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.port", port);
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.user", userName);
+
+		Session session = Session.getDefaultInstance(properties);
+
+		Message msg = new MimeMessage(session);
+
+		msg.setFrom(new InternetAddress(userName));
+		InternetAddress[] toAddresses = { new InternetAddress(toAddress) };
+		msg.setRecipients(Message.RecipientType.TO, toAddresses);
+		msg.setSubject(subject);
+		msg.setSentDate(new Date());
+		msg.setText(message);
+
+		Transport t = session.getTransport("smtp");
+		t.connect(userName, password);
+		t.sendMessage(msg, msg.getAllRecipients());
+		t.close();
+
+	}
+}
